69cd4591fbfd736e9d3a38d62f75ded0b4367afc
Iván López <lopezi@objectcomputing.com>
2020-12-03 08:48:58 +0100

    Make sure both server and client are built or the execution fails

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,5 +1,5 @@
-./gradlew server:assemble
-native-image --no-fallback --class-path server/build/libs/server-*-all.jar
+./gradlew server:assemble && \
+native-image --no-fallback --class-path server/build/libs/server-*-all.jar && \
 
-./gradlew client:assemble
+./gradlew client:assemble && \
 native-image --no-fallback --class-path client/build/libs/client-*-all.jar
